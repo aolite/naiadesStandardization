7a250ad225067e17fa3f50b10d7bb4f08776c71d
aolite <aitorcorchero@gmail.com>
2020-01-12 12:29:52 +0100

    Incorporation of the WordCloud using D3.js

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -5,16 +5,30 @@ const Context = React.createContext("");
 
 function ContextProvider (props){
     const [data, setData] = useState([]);
+    const [wordCloudData, setWordCloudData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    const createWordCloud = (data) => {
+        return data.map(item => {
+            return {
+                text: item.title,
+                url: item.url,
+                value: item.title.length
+            }
+        });
+    };
 
     useEffect(()=>{
         setIsLoading(true);
         setData (Data);
+        const updateData = createWordCloud(Data);
+        console.log(updateData);
+        setWordCloudData(updateData);
         setIsLoading(false)
     }, []);
 
     return (
-        <Context.Provider value={{data,isLoading}}>
+        <Context.Provider value={{data,wordCloudData,isLoading}}>
             {props.children}
         </Context.Provider>
     )
--- src/pages/Landscape.js
+++ src/pages/Landscape.js
@@ -1,14 +1,32 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Map from "../components/Map";
 import {MapProvider} from "../context/MapProvider";
 import {Context} from "../context/ContextProvider";
+import WordCloud from "../components/WordCloud";
 
 function Landscape(){
-    const {data} = useContext(Context);
+    const {data, wordCloudData} = useContext(Context);
+    const fontSizeMapper = word => Math.log2(word.value) * 5;
+    const rotate = word => 0;
 
     return (
         <main>
-            <h2>Landscape Page</h2>
+            <div>
+
+                <p className={'paragraph'}>
+                    <span className={'capitalLetter'}><b>"</b></span> The Water Standardization Landscape is a data observatory of current water standards and initiatives
+                    related to current interoperability & standardization framework. The Water Standardization Landscape
+                    currently stores information about ICT standards, data exchange formats and semantic models. <span className={'capitalLetter'}><b>"</b></span>
+                </p>
+            </div>
+
+            <WordCloud
+                data={wordCloudData}
+                fontSizeMapper={fontSizeMapper}
+                rotate={rotate}
+                onWordClick={(word)=> window.location.assign(word.url) }
+            />
+
             <MapProvider>
                 <Map
                     data = {data}
